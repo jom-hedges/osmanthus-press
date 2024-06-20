ae179bf7c2bf88ed2b8c6feeb99df871701302b5
jom-hedges <jom_hedges@proton.me>
2024-06-20 13:50:05 +0800

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import markdoc from "@astrojs/markdoc";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
+});
