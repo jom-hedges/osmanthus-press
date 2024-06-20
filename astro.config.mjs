273624f975d620b32d6e529ef7a562908a90eacc
jom-hedges <jom_hedges@proton.me>
2024-06-20 15:42:28 +0800

    worked on footer, configuring the DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import markdoc from "@astrojs/markdoc";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), mdx(), sitemap()],
+  integrations: [react(), markdoc(), mdx(), sitemap(), db()],
   output: "server",
   adapter: vercel()
 });
---
+++ db/config.ts
@@ -0,0 +1,14 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Email = defineTable({
+  columns: {
+    columns: {
+      email: column.text()
+    }
+  }
+})
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: { Email }
+});
