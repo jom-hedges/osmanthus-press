0f19f3e031ef24f7dccc3f6a5416fb452ec3534e
jom-hedges <jom_hedges@proton.me>
2024-06-23 15:42:35 +0800

    added a missing comma

--- db/seed.ts
+++ db/seed.ts
@@ -3,7 +3,7 @@ import { db, Email } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Email).values([
-		{ email: 'osmanthuspress@gmail.com' }
+		{ email: 'osmanthuspress@gmail.com' },
 		{ email: 'test@osmanthus.tv' }
 	])
 }
