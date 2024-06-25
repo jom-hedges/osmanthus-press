e6c588d0f33650db6cbe62ad644775b9d58d00aa
jom-hedges <jom_hedges@proton.me>
2024-06-25 10:08:03 +0800

    Astro DB and my skill issue couldn't process the env variables. Something was missing in the docs, and my skill issue is not fully there.

--- db/seed.ts
+++
@@ -1,9 +0,0 @@
-import { db, Email } from 'astro:db';
-
-// https://astro.build/db/seed
-export default async function seed() {
-	await db.insert(Email).values([
-		{ email: 'osmanthuspress@gmail.com' },
-		{ email: 'test@osmanthus.tv' }
-	])
-}
